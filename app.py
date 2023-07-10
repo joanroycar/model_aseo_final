50c3630934000eb41524763e921d3957f05fa0bc
JOAN CASA SJL <joanroycar@gmail.com>
2023-07-09 19:55:37 -0500

    forecast

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import pickle
 import pandas as pd
-with open('forecast_aseo_of.pckl', 'rb') as fin:
+with open('forecast_aseo_ofi1.pckl', 'rb') as fin:
     m2 = pickle.load(fin)
 
 from flask import Flask, jsonify, request
